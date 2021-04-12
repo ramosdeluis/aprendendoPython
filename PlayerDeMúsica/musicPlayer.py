c192298de9be8b6a53a49ef47e4161ab1092d95d
ramosdeluis <alberto.rir@icloud.com>
2021-04-12 16:46:47 -0300

    Primeiro protótipo de player

---
+++ PlayerDeMúsica/musicPlayer.py
@@ -0,0 +1,103 @@
+from pygame import mixer
+from tkinter import Tk
+from tkinter import Label
+from tkinter import Button
+from tkinter import filedialog
+
+current_volume = float(0.4)
+
+
+# Functions
+def play_song():
+    filename = filedialog.askopenfilename(initialdir='c:/', title='Por favor, selecione um arquivo.')
+    current_song = filename
+    song_title = filename.split('/')
+    song_title = song_title[-1]
+
+    try:
+        mixer.init()
+        mixer.music.load(current_song)
+        mixer.music.set_volume(current_volume)
+        mixer.music.play()
+        song_title_label.config(fg='green', text='Tocando: ' + str(song_title))
+        volume_label.config(fg='green', text='Volume: ' + str(current_volume))
+    except Exception as e:
+        print(e)
+        song_title_label.config(fg='red', text='Erro ao tocar a música.')
+
+
+def reduce_volume():
+    try:
+        global current_volume
+        if current_volume <=0:
+            volume_label.config(fg='red', text='Volume: Mutado')
+            return
+        current_volume -= float(0.1)
+        current_volume = round(current_volume, 1)
+        mixer.music.set_volume(current_volume)
+        volume_label.config(fg='green', text='Volume: ' + str(current_volume))
+    except Exception as e:
+        print(e)
+        song_title_label.config(fg='red', text='Nenhuma música selecionada')
+
+
+def increase_volume():
+    try:
+        global current_volume
+        if current_volume >=1:
+            volume_label.config(fg='green', text='Volume: Máximo')
+            return
+        current_volume += float(0.1)
+        current_volume = round(current_volume, 1)
+        mixer.music.set_volume(current_volume)
+        volume_label.config(fg='green', text='Volume: ' + str(current_volume))
+    except Exception as e:
+        print(e)
+        song_title_label.config(fg='red', text='Nenhuma música selecionada')
+
+
+def pause():
+    try:
+        mixer.music.pause()
+    except Exception as e:
+        print(e)
+        song_title_label.config(fg='red', text='Nenhuma música não selecionada!')
+
+
+def resume():
+    try:
+        mixer.music.unpause()
+    except Exception as e:
+        print(e)
+        song_title_label.config(fg='red', text='Nenhuma música selecionada!')
+
+
+
+
+# Main Screen
+master = Tk()
+master.title('Tocador de Música')
+
+# Labels
+Label(master, text='Tocador de Música Customizado', font=('calibri', 15), fg='red').grid(sticky='N', row=0,padx=120)
+Label(master, text='Por favor, selecione uma música para tocar'
+      , font=('calibri', 12), fg='blue').grid(sticky='N', row=1)
+Label(master, text='Volume'
+      , font=('calibri', 12), fg='red').grid(sticky='N', row=4)
+
+song_title_label = Label(master, font=('calibri', 12))
+song_title_label.grid(stick='N', row=3)
+volume_label = Label(master, font=('calibri', 12))
+volume_label.grid(sticky='N', row=5)
+
+# Buttons
+Button(master, text='Selecione uma Música', font=('calibri', 12), command=play_song).grid(row=2, sticky='N')
+Button(master, text='Pausar', font=('calibri', 12), command=pause).grid(row=3, sticky='E')
+Button(master, text='Retomar', font=('calibri', 12), command=resume).grid(row=3, sticky='W')
+Button(master, text='+', font=('calibri', 12), width=5, command=increase_volume).grid(row=5, sticky='E')
+Button(master, text='-', font=('calibri', 12), width=5, command=reduce_volume).grid(row=5, sticky='W')
+
+
+
+master.mainloop()
+
