042130c2e8a6b194e1dac84c2b7b1491f8f8ff2d
ramosdeluis <alberto.rir@icloud.com>
2021-04-27 09:34:33 -0300

    Usando a API do Twitter. Fazendo posts e procurando por palavras.

---
+++ Alura/Dados/Twitter/main.py
@@ -0,0 +1,13 @@
+﻿from oo import Requisição
+
+while True:
+    opção = str(input('[1] - Fazer Tweet\n[2] - Pesquisar termos\n')).strip()
+    if opção == '1' or opção == '2':
+        if opção == '1':
+            Requisição().cria_requisição('1')
+        else:
+            Requisição().cria_requisição('2').formata()
+    else:
+        print('\033[31mOpção inválida, tente novamente!\033[m')
+
+print('\033[34mObrigado por usar o programa!!\003[m')
---
+++ Alura/Dados/Twitter/oo.py
@@ -0,0 +1,64 @@
+﻿import pprint
+
+import oauth2
+import json
+import urllib.parse
+
+
+class Requisição:
+    def __init__(self):
+        self.cliente = self.gera_cliente()
+
+    def cria_requisição(self, opção):
+        if opção == '1':
+            return Posta(self.cliente)
+        elif opção == '2':
+            return Pesquisa(self.cliente)
+
+    def gera_cliente(self):
+        api_key = ''
+        api_secret_key = ''
+        token_key = ''
+        token_secret_key = ''
+        consumer = oauth2.Consumer(api_key, api_secret_key)
+        token = oauth2.Token(token_key, token_secret_key)
+        cliente = oauth2.Client(consumer, token)
+        return cliente
+
+
+class Posta:
+    def __init__(self, cliente):
+        self.cliente = cliente
+        self.texto = self.solicita_texto()
+        self.requisita(self.texto)
+
+    def solicita_texto(self):
+        query = input('O que queres postar, @luisraamons? ')
+        query_codificado = urllib.parse.quote(query, safe='')
+        return query_codificado
+
+    def requisita(self, query_codificado):
+        requisicao = self.cliente.request('https://api.twitter.com/1.1/statuses/update.json?status=' + query_codificado,
+                                          method='POST')
+        return requisicao
+
+class Pesquisa:
+    def __init__(self, cliente):
+        self.cliente = cliente
+        self.objeto = self.requisita(self.solicita_texto())
+
+    def solicita_texto(self):
+        query = input('O que queres pesquisar no Twitter? ')
+        query_codificado = urllib.parse.quote(query, safe='')
+        return query_codificado
+
+    def requisita(self, query_codificado):
+        requisicao = self.cliente.request('https://api.twitter.com/1.1/search/tweets.json?q=' + query_codificado + '&lang=pt')
+        decodificado = requisicao[1].decode()
+        objeto = json.loads(decodificado)
+        return objeto
+
+    def formata(self):
+        twittes = self.objeto['statuses']
+        for twit in twittes:
+            print(f'{"-*-"*30}\nO usuário \033[34m@{twit["user"]["screen_name"]}\033[m diz:\n\033[31m{twit["text"]}\033[m')
