8192df10611586178033658d83afb6f261b42c1a
ramosdeluis <alberto.rir@icloud.com>
2021-04-28 10:57:30 -0300

    Estudando testes.

---
+++ Alura/Dados/leilao/test_dominio.py
@@ -0,0 +1,72 @@
+﻿from unittest import TestCase
+import dominio
+
+
+class Leiloeiro(TestCase):
+    def setUp(self) -> None:
+        self.luís = dominio.Usuario('Luís')
+        self.lance_luís = dominio.Lance(self.luís, 1000)
+        self.leilão = dominio.Leilao('Celular')
+        self.leilão.lances.append(self.lance_luís)
+
+    def test_quando_colocamos_o_menor_lance_primeiro_deve_retornar_o_maior_e_menor_corretamente(self):
+        ramos = dominio.Usuario('Ramos')
+        lance_ramos = dominio.Lance(ramos, 500)
+        self.leilão.lances.append(lance_ramos)
+        self.leilão.lances.append(self.lance_luís)
+
+        avaliador = dominio.Leiloeiro()
+        avaliador.avalia(self.leilão)
+
+        menor_valor_esperado = 500
+        maior_valor_esperado = 1000
+
+        self.assertEqual(menor_valor_esperado, avaliador.menor_valor)
+        self.assertEqual(maior_valor_esperado, avaliador.maior_valor)
+
+    def test_quando_colocamos_o_maior_lance_primeiro_deve_retornar_o_maior_e_menor_corretamente(self):
+        ramos = dominio.Usuario('Ramos')
+        lance_ramos = dominio.Lance(ramos, 500)
+
+        self.leilão.lances.append(self.lance_luís)
+        self.leilão.lances.append(lance_ramos)
+
+        avaliador = dominio.Leiloeiro()
+        avaliador.avalia(self.leilão)
+
+        menor_valor_esperado = 500
+        maior_valor_esperado = 1000
+
+        self.assertEqual(menor_valor_esperado, avaliador.menor_valor)
+        self.assertEqual(maior_valor_esperado, avaliador.maior_valor)
+
+    def test_quando_colocamos_somente_um_valor_deve_retornar_maior_e_menor_iguais_ao_mesmo(self):
+        self.leilão.lances.append(self.lance_luís)
+
+        avaliador = dominio.Leiloeiro()
+        avaliador.avalia(self.leilão)
+
+        menor_valor_esperado = 1000
+        maior_valor_esperado = 1000
+
+        self.assertEqual(menor_valor_esperado, avaliador.menor_valor)
+        self.assertEqual(maior_valor_esperado, avaliador.maior_valor)
+
+    def test_quando_colocamos_mais_de_dois_valores_deve_retornar_o_maior_e_menor_corretamente(self):
+        ramos = dominio.Usuario('Ramos')
+        lance_ramos = dominio.Lance(ramos, 500)
+        nunes = dominio.Usuario('Ramos')
+        lance_nunes = dominio.Lance(ramos, 900)
+
+        self.leilão.lances.append(self.lance_luís)
+        self.leilão.lances.append(lance_ramos)
+        self.leilão.lances.append(lance_nunes)
+
+        avaliador = dominio.Leiloeiro()
+        avaliador.avalia(self.leilão)
+
+        menor_valor_esperado = 500
+        maior_valor_esperado = 1000
+
+        self.assertEqual(menor_valor_esperado, avaliador.menor_valor)
+        self.assertEqual(maior_valor_esperado, avaliador.maior_valor)
---
+++ Alura/brincando.py
@@ -0,0 +1,48 @@
+﻿class Moeda:
+    @staticmethod
+    def gera_moeda(código, valor):
+        if código == 1:
+            return EuroReal(valor)
+        elif código == 2:
+            return RealEuro(valor)
+
+
+class RealEuro:
+    def __init__(self, valor):
+        self.valor = valor
+        self.valor_convertido = self.converte()
+
+    def __str__(self):
+        return self.formata()
+
+    def converte(self):
+        valor_convertido = self.valor / 6.56
+        return valor_convertido
+
+    def formata(self):
+        return f'R$ {self.valor:,.2f} = € {self.valor_convertido:,.2f}'\
+            .replace(',', '_').replace('.',',').replace('_','.')
+
+
+class EuroReal:
+    def __init__(self, valor):
+        self.valor = valor
+        self.valor_convertido = self.converte()
+
+    def __str__(self):
+        return self.formata()
+
+    def converte(self):
+        valor_convertido = self.valor * 6.56
+        return valor_convertido
+
+    def formata(self):
+        return f'€ {self.valor:,.2f} = R$ {self.valor_convertido:,.2f}'\
+            .replace(',', '_').replace('.',',').replace('_','.')
+
+
+
+euros = Moeda.gera_moeda(1, 90000000)
+reais = Moeda.gera_moeda(2, 1200000)
+print(reais)
+print(euros)
